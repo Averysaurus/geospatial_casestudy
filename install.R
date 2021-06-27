cb82456447c3b9832f8996a472ddaf0c2d12346a
Avery Richards <43361193+Averysaurus@users.noreply.github.com>
2021-06-26 18:16:41 -0700

    Create install.R
    package installer my containers

---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages("pacman", "RSocrata", "tidyverse", "lubridate", "ggplot2", "leaflet", "sf", 
+  "tmap", "rgdal", "prettydoc", "hrbrthemes", "raster", "sp", "spdep", 
+  "spatstat", "fields", "car", "pgirmess", "geoR")
